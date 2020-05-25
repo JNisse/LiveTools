c2a31c583028e07d3abee0fb5a654cb3b4c71448
Bolger Lightfoot <43555135+BolgerLightfoot@users.noreply.github.com>
2020-05-25 21:27:40 +0200

    Lottning till PGN
    För att hämta lottning i medlemssystemet till en PGN fil.

---
+++ webparsepy.py
@@ -0,0 +1,45 @@
+import requests
+from bs4 import BeautifulSoup
+
+  
+def f(l,url):
+    
+    resp=requests.get(url)
+    b=False
+    if resp.status_code==200:
+        
+        soup=BeautifulSoup(resp.text,'html.parser')
+        g=soup.find_all('span')
+        contents=[]
+        results=[]
+        for i in g:
+            contents.append(str(i))
+        for j in contents:
+            j=j[6:len(j)-7]
+            if j=='RESULTAT':
+                b=True 
+            if b and j!='RESULTAT' and len(j)>5:
+               results.append(j)
+    else:
+        print ("Error")
+    for k in range (len(results)//2):
+        l.append(results[2*k])
+        l.append(results[2*k+1])
+        print (results[2*k] + " - " + results[2*k+1])
+def PGN(lista):
+    with open ('lottning.pgn','w') as f:
+        for k in range(len(lista)//2):
+            f.write("[White\""+str(lista[2*k])+"\"]\n")
+            f.write("[Black\""+str(lista[2*k+1])+"\"]\n")
+            f.write("\n*\n")
+
+lista=[]
+url=input("Kopiera in webbadressen till turneringen")
+f(lista,url)
+PGN(lista)
+print("done")
+input('Press ENTER to exit')
+
+
+        
+ 
